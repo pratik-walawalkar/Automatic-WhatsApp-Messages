a171b0502bccb6683652487615009b363915b42e
pratik-walawalkar <pratik-walawalkar@users.noreply.github.com>
2021-01-21 20:19:40 +0100

    Update auto_WA_msg.py

--- auto_WA_msg.py
+++ auto_WA_msg.py
@@ -13,21 +13,28 @@ class send_message:
     
 
     current = datetime.now()
-    current_hour = current.strftime("%H") 
+    current_hour = int(current.strftime("%H")) 
     current_minutes = int(current.strftime("%M"))+1
 
     
     def contact_info():
-        number = "+91123456789"                # Enter contact no. or group id here (group id is available in group link)
+        
+        '''
+        Enter the time in 24-hours fromat, for example - 5.28 Pm. 
+        hours = 17
+        minutes = 28
+        '''
+        
+        number = "+9112345678921"                # Enter contact no. or group id here (group id is available in group link)
         message = "Test message using python"    # Enter the message to be sent
         hours = send_message.current_hour        # Enter hours
         minutes = send_message.current_minutes   # Enter minutes        
 
         send_message.send_msg(number, message, hours, minutes)
             
-    def send_msg(contact, message, hours , minutes):
+    def send_msg(contact, message, hours, minutes):
 
-        whatsapp.sendwhatmsg(contact, message, hours, minutes )
+        whatsapp.sendwhatmsg(contact, message, hours, minutes)
         
 
 
